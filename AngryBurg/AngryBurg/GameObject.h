9cb869f0bc4e71432f9be56fa729101fd4b580db
Henry Oliver <henry983615@gmail.com>
2019-09-21 16:33:15 +1200

    GameObject Class

---
+++ AngryBurg/AngryBurg/GameObject.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Util.h"
+
+class RenderClass {
+public:
+	virtual void Render(Transform* _transform) = 0;
+	virtual void SetTexture(GLuint _tex) = 0;
+	virtual void SetShader(GLuint _shader) = 0;
+};
+
+class NoRender : public RenderClass {
+
+};
+
+class TickClass {
+public:
+	virtual void Tick(float deltaTime, GameObject* _gameObject) = 0;
+};
+
+
+class GameObject {
+public:
+	GameObject();
+	GameObject(RenderClass* r, TickClass* t, Transform _trans, string _name) : _r(r), _t(t), transform(_trans), name(_name) { Console_OutputLog(to_wstring("Creating GameObject: " + _name), LOGINFO); };
+
+	virtual void Tick(float deltaTime, GameObject* _gameObject) { _t->Tick(deltaTime, _gameObject); };
+	virtual void Render() { _r->Render(&transform); };
+
+	virtual void SetTexture(GLuint _tex) { _r->SetTexture(_tex); };
+	virtual void SetShader(GLuint _shader) { _r->SetTexture(_shader); };
+
+	Transform& GetTransform() { return transform; };
+
+protected:
+	Transform transform;
+	RenderClass* _r;
+	TickClass* _t;
+
+	string name;
+};
+
