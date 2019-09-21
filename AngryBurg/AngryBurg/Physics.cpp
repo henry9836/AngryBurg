65a8ff76f0defd9a39d5fa0b9c9793e466d63611
Vaughan Webb <vaughanw2012@gmail.com>
2019-09-21 16:36:14 +1200

    gf

--- AngryBurg/AngryBurg/Physics.cpp
+++ AngryBurg/AngryBurg/Physics.cpp
@@ -8,8 +8,25 @@ void Physics::worldsetup()
 
 	//creating a world
 	b2Vec2 gravity(0.0f, -9.81f);
-	b2World world(gravity);	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(0.0f, -10.0f);	b2Body* groundBody = world.CreateBody(&groundBodyDef);	b2PolygonShape groundBox;
-	groundBox.SetAsBox(50.0f, 10.0f);
-	groundBody->CreateFixture(&groundBox, 0.0f);
+	b2World world(gravity);	b2BodyDef myBodyDef;
+	myBodyDef.type = b2_staticBody;
+	myBodyDef.position.Set(0, 0);
+	b2Body* staticBody = world->CreateBody(&myBodyDef);
+
+	//shape definition
+	b2PolygonShape polygonShape;
+
+	//fixture definition
+	b2FixtureDef myFixtureDef;
+	myFixtureDef.shape = &polygonShape;
+
+	//add four walls to the static body
+	polygonShape.SetAsBox(20, 1, b2Vec2(0, 0), 0);//ground
+	staticBody->CreateFixture(&myFixtureDef);
+	polygonShape.SetAsBox(20, 1, b2Vec2(0, 40), 0);//ceiling
+	staticBody->CreateFixture(&myFixtureDef);
+	polygonShape.SetAsBox(1, 20, b2Vec2(-20, 20), 0);//left wall
+	staticBody->CreateFixture(&myFixtureDef);
+	polygonShape.SetAsBox(1, 20, b2Vec2(20, 20), 0);//right wall
+	staticBody->CreateFixture(&myFixtureDef);
 }
